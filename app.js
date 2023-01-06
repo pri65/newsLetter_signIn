55f216d65b47e5bcfc8c227514212211141b6169
pri65 <priyankasoniii65@gmail.com>
2023-01-06 19:54:50 +0530

    New commands

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", function(req,res) {
+    res.sendFile(__dirname + "/signup.html")
+})
 
 app.listen(3000, function() {
     console.log("Server is running on port 3000")
